1539c7bb79b4856883661009077109a21b80903a
Trajche Petrov <skechboy@gmail.com>
2011-11-10 00:07:29 +0100

    Added basic content listener for inserting choosen glyph into selected input element

---
+++ js/content.js
@@ -0,0 +1,19 @@
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+	
+	var focusEl = window.top.document.activeElement;
+	var text	= request.glyph;
+	if (focusEl.tagName.toLowerCase() == 'input' || focusEl.tagName.toLowerCase() == 'textarea')
+	{
+		var startPos	= focusEl.selectionStart;
+		var endPos		= focusEl.selectionEnd;
+
+		// insert text
+		focusEl.value = focusEl.value.substring(0, startPos)
+							+ text
+							+ focusEl.value.substring(endPos, focusEl.value.length);
+
+		// update current position
+		focusEl.setSelectionRange(startPos + text.length, startPos + text.length);
+		
+	}
+});
