c8e022808bafc8340e716b81dc4d61c101172d13
Aleksandar Jovanov <ace92bt@gmail.com>
2011-11-10 22:51:13 +0100

    Added glyph

--- js/glyphs.js
+++ js/glyphs.js
@@ -3,6 +3,6 @@ var glyphs =
     curency:["&pound;","&yen;","&euro;","&cent","$","&curren;","&#8355;","&#8356;",
              "&#8359;","&#37;","&#137;"],
     math:["<",">"],
-    chars:[],
+    chars:[""],
     signs:[]
 }
