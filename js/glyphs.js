aea444f7e50ccd02fcaf3ab6e3ee17bee4e2e45c
Aleksandar Jovanov <ace92bt@gmail.com>
2011-11-10 21:33:32 +0100

    Added glyph in math category

--- js/glyphs.js
+++ js/glyphs.js
@@ -2,7 +2,7 @@ var glyphs =
 {
     curency:["&pound;","&yen;","&euro;","&cent","$","&curren;","&#8355;","&#8356;",
              "&#8359;","&#37;","&#137;"],
-    math:[],
+    math:["<"],
     chars:[],
     signs:[]
 }
